7eb5d5dd4328c27b0b7a1702022e1fd3173e5922
Neville Rabakale <neville.rabakale@gmail.com>
2022-12-08 22:34:54 +0100

    Added buttons to link to the correct pages

--- EmployeesMvc/Models/DataService.cs
+++ EmployeesMvc/Models/DataService.cs
@@ -5,6 +5,8 @@
         static List<Employee> Employees = new List<Employee>
         { 
             new Employee { Id = 0, Name= "Jon", Email = "jon@gmail.com"},
+            new Employee { Id = 1, Name= "Boli", Email = "boli@gmail.com"},
+            new Employee { Id = 2, Name= "Anne", Email = "anne@gmail.com"},
         };
 
         public void AddEmployee(Employee employee)
