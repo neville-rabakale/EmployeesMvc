3468dc31f46ef582bc364c1e68151e8f38fb2103
Neville Rabakale <neville.rabakale@gmail.com>
2022-12-09 15:26:47 +0100

    Added layout, TimeParial (the date), ViewImport, and ViewStart. Also added bootstrap for styles

--- EmployeesMvc/Models/DataService.cs
+++ EmployeesMvc/Models/DataService.cs
@@ -4,7 +4,7 @@ namespace EmployeesMvc.Models
 {
     public class DataService
     {
-        List<Employee> Employees = new List<Employee>
+        static List<Employee> Employees = new List<Employee>
         { 
             new Employee { Id = 0, Name= "Jon", Email = "jon@gmail.com"},
             new Employee { Id = 1, Name= "Boli", Email = "boli@gmail.com"},
@@ -14,10 +14,16 @@ namespace EmployeesMvc.Models
         public void AddEmployee(Employee employee)
         {
             employee.Id = Employees.Max(x => x.Id) + 1;
+
+            Employees.Add(employee);
+        }
+
+        public void AddEmployeeJson(Employee employee)
+        {
+            employee.Id = Employees.Max(x => x.Id) + 1;
             string json = JsonSerializer.Serialize(employee);
             string fileName = "EmployeeData.js";
             File.WriteAllText(fileName, json);
-            //Employees.Add(employee);
         }
         public Employee[] GetAllEmployees()
         {
--- EmployeesMvc/Models/Employee.cs
+++ EmployeesMvc/Models/Employee.cs
@@ -6,10 +6,10 @@ namespace EmployeesMvc.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please enter Name")]
         public string Name { get; set; }
-        [Required]
         [EmailAddress]
+        [Required(ErrorMessage = "Please enter Email")]
         [Display(Name="Enter Email")]
         public string Email { get; set; }
     }
